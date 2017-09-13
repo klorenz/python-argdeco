db0cdf32c2e9cdefdbc09a0d3d08682140be4436
Kay-Uwe (Kiwi) Lorenz <kiwi@franka.dyndns.org>
2017-09-13 23:43:56 +0200

    increase version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='argdeco',
-  version='1.0.1',
+  version='1.0.2',
   py_modules=['argdeco'],
   author = 'Kay-Uwe (Kiwi) Lorenz',
   author_email = "kiwi@franka.dyndns.org",
