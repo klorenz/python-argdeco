28245b1271cb2bd9ce70e9d63e26e5a41d3577a0
Kay-Uwe (Kiwi) Lorenz <kiwi@moduleworks.com>
2020-09-21 23:43:34 +0200

    for now use plain text for readme

--- setup.py
+++ setup.py
@@ -10,13 +10,14 @@ setup(name='argdeco',
   #py_modules=['argdeco'],
   packages = ['argdeco'],
   author = 'Kay-Uwe (Kiwi) Lorenz',
-  author_email = "kiwi@franka.dyndns.org",
+  author_email = "tabkiwi@gmail.com",
   url = 'https://github.com/klorenz/python-argdeco',
   description = "specify command arguments in function decorator",
   install_requires=[
     'argcomplete'
   ],
-  long_description_content_type = "text/markdown",
+  #long_description_content_type = "text/markdown",
+  long_description_content_type = "text/plain",
   long_description = long_description,
   keywords = "argument command argparse cli",
   #download_url
